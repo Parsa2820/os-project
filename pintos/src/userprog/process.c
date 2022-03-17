bc1e0a54a221c1fd996fc4d20a89611954fec2aa
Parsa Mohammadian <parsa2820@gmail.com>
2022-03-17 17:35:34 +0000

    Deny write to currently executable file

--- pintos/src/userprog/process.c
+++ pintos/src/userprog/process.c
@@ -365,6 +365,8 @@ bool load(const char *file_name, void (**eip)(void), void **esp)
     goto done;
   }
 
+  file_deny_write(file);
+
   /* Read and verify executable header. */
   if (file_read(file, &ehdr, sizeof ehdr) != sizeof ehdr || memcmp(ehdr.e_ident, "\177ELF\1\1\1", 7) || ehdr.e_type != 2 || ehdr.e_machine != 3 || ehdr.e_version != 1 || ehdr.e_phentsize != sizeof(struct Elf32_Phdr) || ehdr.e_phnum > 1024)
   {
